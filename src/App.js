de2ff0e453d99feac22ab9e8d6d4969c69d6328b
karun74 <sknithi@yahoo.com>
2021-04-07 16:36:52 -0500

    third commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Navigation, Footer, Home, About, Contact, Compute, KubernetesSizing, RDSPriceForm, EC2PriceForm, CostPerApplnForgate, CostPerApplnEKS, MigrationCost } from "./Components";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path="/RDSPriceForm" exact component={() => <RDSPriceForm />} />
+          <Route path="/EC2PriceForm" exact component={() => <EC2PriceForm />} />
+          <Route path="/CostPerApplnForgate" exact component={() => <CostPerApplnForgate />} />
+	  <Route path="/CostPerApplnEKS" exact component={() => <CostPerApplnEKS />} />
+	  <Route path="/MigrationCost" exact component={() => <MigrationCost />} />
+	  <Route path="/KubernetesSizing" exact component={() => <KubernetesSizing />} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
